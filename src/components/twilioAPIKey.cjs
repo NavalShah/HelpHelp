a88e46149443079a0f113f9aafa10cce60843085
Aarush Pawar <aarush711pawar@gmail.com>
2025-03-22 16:08:21 -0400

    api keys

---
+++ src/components/twilioAPIKey.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    TWILIO_ACCOUNT_SID: "ACcca6a350753b0dd706ebf1cf1e5f0ceb",
+    TWILIO_AUTH_TOKEN: "afbacc7925c173e59b649ae0d234ffb8",
+    TWILIO_PHONE_NUMBER: "+1 888 998 0496",
+    MY_NUMBER: "+18148762220"
+};
